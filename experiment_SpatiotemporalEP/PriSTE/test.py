288b84ee17fb1bb470e543de6d23019bf7ed7965
Han_naH <554579934@qq.com>
2020-11-16 21:46:41 +0800

    线性规划部分

--- experiment_SpatiotemporalEP/PriSTE/test.py
+++ experiment_SpatiotemporalEP/PriSTE/test.py
@@ -1,79 +1,45 @@
+import pulp as lp
 import numpy as np
 
-filename = "./groundtruth/56_times_30.csv"
-T = np.loadtxt(filename,delimiter=",")
-
-avg = []
-mid = []
-for i in range(len(T)):
-    avg.append(np.mean(T[i]))
-    mid.append(np.median(T[i]))
+R=57
+p = 1/R
+epsilon = 4
 
 
+U = np.loadtxt("./uncertainty_matrix/temp336cycles/rse_uncertainty_matrix.csv",delimiter=",")
+(m,n) = U.shape
 
-class Map():
-    def __init__(self):
-        self.map = self.setMap()
+flatten = lambda x: [y for l in x for y in flatten(l)] if type(x) is list else [x]
 
-    def setMap(self):
-        map = np.empty((7, 8), int)
-        for i in range(7):
-            for j in range(8):
-                map[i, j] = i * 8 + j
-        return map
+PB = lp.LpProblem(name="Obufuscation_Probility",sense=lp.LpMinimize)
 
-    def getMap(self):
-        return self.map
+P = lp.LpVariable.matrix("obfuscation_probility",(range(m),range(n)),lowBound=0)
 
-def setZb(map:np.ndarray):
-    zb = []
-    for i in range(len(map[:,0])):
-        for j in range(len(map[0])):
-            zb.append((i,j))
-    return zb
+x = flatten(P)
 
-def getMD(map:np.ndarray,zb:list):
-    MD = []
-    for k in range(map.size):
-        md = np.ndarray(map.shape)
-        for i in range(map.shape[0]):
-            for j in range(map.shape[1]):
-                md[i,j] = int(abs(zb[k][0]-i)+abs(zb[k][1]-j))
-        MD.insert(k,md)
-    return MD
-
-def getTD(T:np.ndarray,map:np.ndarray):
-    mid = []
-    TD = np.ndarray((map.size,map.size))
-    for i in range(len(T)):
-        mid.append(np.median(T[i]))
-    for i in range(TD.shape[0]):
-         for j in range(TD.shape[1]):
-             TD[i, j] = mid[i]/mid[j]
-    return TD
-
-def getOD(map:np.ndarray,zb:list):
-    OD = []
-    for k in range(map.size):
-        od = np.ndarray(map.shape)
-        for i in range(map.shape[0]):
-            for j in range(map.shape[1]):
-                od[i,j] = np.sqrt((zb[k][0]-i)**2+(zb[k][1]-j)**2)
-        OD.insert(k,od)
-    return OD
-
-Map = Map()
-map = Map.getMap()
-zb = setZb(map)
-MD = getMD(map,zb)
-OD = getOD(map,zb)
-# print(1/(OD[0]+1))
-for k in range(len(OD)):
-    OD[k] = 1/(OD[k]+1)
-    # for i in range(OD[k].shape[0]):
-    #     s = np.sum(OD[k])
-    #     for j in range(OD[k].shape[1]):
-    #         OD[k][i,j] /= s
+PB += lp.lpSum(lp.lpDot(p*U[:,j],[a[j] for a in P]) for j in range(n))
 
 
-TD = getTD(T,map)
+# PB += lp.lpSum(lp.lpDot(p,lp.lpSum(lp.lpDot(U[i,j],P[i][j]) for j in range(n))) for i in range(n))
+
+# for i in range(m):
+#     PB += lp.lpDot(p,lp.lpSum(lp.lpDot(U[i,j],P[i,j]) for j in range(n)))
+
+for i in range(len(x)):
+    for j in range(len(x)):
+        PB.addConstraint(lp.lpDot(epsilon,x[j])>=x[i])
+
+
+for j in range(n):
+    PB+=(lp.lpSum(lp.lpDot(p,P[:][j]))==1/R)
+
+for i in range(m):
+    PB+=(lp.lpSum(P[i][j] for j in range(n))==1)
+
+PB.solve()
+
+res = np.ndarray((57,57))
+for i in range(len(P)):
+    for j in range(len(P[0])):
+        res[i,j] = lp.value(P[i][j])
+
